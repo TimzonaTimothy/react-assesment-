952836fe88979d974c0b6713546926cb3c494976
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-08-24 13:07:11 +0100

    first commit

---
+++ build/precache-manifest.601a02995e4d3fb8972d16fa53f015e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfd0ccef26b03fca9ed2f07117cd439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7775ca0ffaf3cf8c74b1",
+    "url": "/static/css/main.b226e0b5.chunk.css"
+  },
+  {
+    "revision": "ada2c03c0125937f0ed6",
+    "url": "/static/js/2.aeeb7782.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.aeeb7782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7775ca0ffaf3cf8c74b1",
+    "url": "/static/js/main.f79c7336.chunk.js"
+  },
+  {
+    "revision": "866ed326f9b633faeba8",
+    "url": "/static/js/runtime-main.0d63bdcd.js"
+  }
+]);
